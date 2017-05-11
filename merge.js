ab4ecebe53a9e85f3ce22b998353fdb2c45a5cac
David Noriega <dnori817@gmail.com>
2017-05-11 15:16:39 -0400

    assignment completed - bonus pending

--- merge.js
+++ merge.js
@@ -2,12 +2,6 @@ const fs = require("fs");
 const people1 = require("./people/people1.json");
 const people2 = require("./people/people2.json");
 
-// console.log(people1, people2);
-
-let merge = people1.concat(people2).sort();
-
-// console.log(merge);
-
 // fs.readdir("./people", function(err, files) {
 // 	if (err) {
 // 		console.error("Unable to read directory");
@@ -21,12 +15,15 @@ let merge = people1.concat(people2).sort();
 // // Print each file's name
 // 	// console.log("All files in directory:");
 // });
+
+let merge = people1.concat(people2).sort();
+let string = "";
+
 for (let i = 0; i < merge.length; i++) {
-	merge[i];
-	console.log(merge[i]);
+	string += merge[i] + "\n";
 }
 
-fs.writeFile("./sorted_people.txt", JSON.stringify(merge), function(err) {
+fs.writeFile("./sorted_people.txt", string, function(err) {
 	if (err) {
 		console.error("Could not save ");
 		console.error(err);
