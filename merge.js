a3364aab3f488caa8a463e8b17273f76b5803af5
David Noriega <dnori817@gmail.com>
2017-05-11 11:06:30 -0400

    merge and sort arrays

--- merge.js
+++ merge.js
@@ -1,1 +1,9 @@
 const fs = require("fs");
+const people1 = require("./people1.json");
+const people2 = require("./people2.json");
+
+// console.log(people1, people2);
+
+let merge = people1.concat(people2).sort();
+
+console.log(merge);
